71035f7e5a2207ebafa7993b2cfac7720ab08447
archer <1246506786@qq.com>
2017-04-01 21:46:46 +0800

    misc commit

--- src/mq_broker.py
+++ src/mq_broker.py
@@ -1,6 +1,18 @@
+import logging
+import asyncio
+import os
+from hbmqtt.broker import Broker
 
 
 class MqBroker(object):
 	""" message queue broker server """
 	def __init__(self):
-		pass
+		formatter = "[%(asctime)s] :: %(levelname)s :: %(name)s :: %(message)s"
+		logging.basicConfig(level=logging.INFO, format=formatter)
+		asyncio.get_event_loop().run_until_complete(self.broker_coro())
+		asyncio.get_event_loop().run_forever()
+
+	@asyncio.coroutine
+	def broker_coro(self):
+		broker = Broker()
+		yield from broker.start()
