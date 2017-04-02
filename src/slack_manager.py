ef7c3728bb6af3332c74e48c5cc29b3ecaef322a
archer <1246506786@qq.com>
2017-04-02 09:01:48 +0800

    slack_manager mqtt subscriber and publisher seems work

--- src/slack_manager.py
+++ src/slack_manager.py
@@ -1,4 +1,7 @@
 from lib.slack_helper import SlackHelper
+import asyncio
+from hbmqtt.client import MQTTClient, ClientException, ConnectException
+from hbmqtt.mqtt.constants import QOS_1, QOS_2
 
 
 class SlackManager(object):
@@ -11,3 +14,32 @@ class SlackManager(object):
 
 	def run(self):
 		pass
+
+	@asyncio.coroutine
+	def subcriber(self):
+		""" subscribe mqtt topic /slack_in, listen for incoming msgs """
+		C = MQTTClient()
+		yield from C.connect('mqtt://127.0.0.1:1883/')
+		yield from C.subscribe([
+			('/slack_in', QOS_1),
+		])
+		try:
+			while True:
+				message = yield from C.deliver_message()
+				packet = message.publish_packet
+				print("%d:  %s => %s" % (0, packet.variable_header.topic_name, str(packet.payload.data)))
+		except ClientException as ce:
+			yield from C.unsubscribe(['/slack_in'])
+			yield from C.disconnect()
+			print("Client exception: %s" % ce)
+
+	@asyncio.coroutine
+	def publisher(self, msg):
+		""" publish msg to mqtt topic /slack_out """
+		try:
+			C = MQTTClient()
+			yield from C.connect('mqtt://127.0.0.1:1883/')
+			yield from C.publish('/slack_out', bytes(msg, 'utf-8'), qos=0x00)
+			yield from C.disconnect()
+		except ConnectException as ce:
+			asyncio.get_event_loop().stop()
