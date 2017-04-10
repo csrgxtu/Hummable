9cedfd851b8ca4b879f904d6ec33c23d1062d2ea
Archer Li <archer.li@shopeemobile.com>
2017-04-10 15:24:19 +0800

    temp commit

--- src/email.py
+++ src/email.py
@@ -3,6 +3,13 @@ import imaplib
 
 class Email(object):
 	"""listening email event"""
+
+	mail = None
+
 	def __init__(self, address, password):
-		mail = imaplib.IMAP4_SSL('imap.gmail.com')
+		self.mail = imaplib.IMAP4_SSL('imap.gmail.com')
+		self.mail.login(address, password)
+
+	def new_mail_event(self):
+		""" check new mail in a loop """
 		pass
