c79a9ce69a67e983ced7becf0d6188365b5a8315
Archer Li <archer.li@shopeemobile.com>
2017-04-12 19:28:57 +0800

    trying 2 understand git

---
+++ conf/settings.py
@@ -0,0 +1,17 @@
+DEBUG_LEVEL = 'debug'
+
+# bla
+Slack_Url = 'mqtt://127.0.0.1:1883'
+Slack_In_Topic = '/slack_in'
+Slack_Out_Topic = '/slack_out'
+
+# MQ
+MAX_CONNECTIONS = 128
+BIND = '127.0.0.1:1883'
+MQTT_URL = 'mqtt://' + BIND
+TIMEOUT_DISCONNECT_DELAY = 2
+HOST = '127.0.0.1'
+
+# Gmail
+Gmail_Address = 'archer.li@shopeemobile.com'
+Gmail_Password = 'whatthefuck'
